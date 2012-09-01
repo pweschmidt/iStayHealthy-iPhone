9e26f294eb65a55a3025c330901c8f5b8e069ec2
Peter Schmidt <peter.we.schmidt@gmail.com>
2012-09-01 10:41:06 +0100

    major cleanup

---
+++ Classes/GeneralSettings.h
@@ -0,0 +1,18 @@
+/*
+ *  GeneralSettings.h
+ *  iStayHealthy
+ *
+ *  Created by Peter Schmidt on 06/01/2011.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#define TEXTCOLOUR		[UIColor colorWithRed:51.0/255.0 green:102.0/255.0 blue:204.0/255.0 alpha:1.0]
+#define DARK_YELLOW		[UIColor colorWithRed:255.0/255.0 green:152.0/255.0 blue:0.0/255.0 alpha:1.0]
+#define DARK_GREEN		[UIColor colorWithRed:0.0/255.0 green:102.0/255.0 blue:0.0/255.0 alpha:1.0]
+#define DARK_RED		[UIColor colorWithRed:204.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1.0]
+#define BACKGROUND_EVEN [UIColor colorWithRed:224.0/255.0 green:255.0/255.0 blue:255.0/255.0 alpha:1.0]
+#define BACKGROUND_ODD  [UIColor colorWithRed:173.0/255.0 green:224.0/255.0 blue:255.0/255.0 alpha:1.0]
+#define DEFAULT_BACKGROUND      [UIColor colorWithRed:253.0/255.0 green:255.0/255.0 blue:240.0/255.0 alpha:1.0];
+#define TINTCOLOUR		[UIColor colorWithRed:236.0/255.0 green:153.0/255.0 blue:51.0/255.0 alpha:1.0]
+
