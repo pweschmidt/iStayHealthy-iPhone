480ccef1cbc49503dedc07d521355dc7151491ee
Peter Schmidt <peter.we.schmidt@gmail.com>
2013-06-22 13:48:37 +0100

    added storyboards and container view controller for version 4

---
+++ Classes/Controllers/ContainerViewController.h
@@ -0,0 +1,14 @@
+//
+//  ContainerViewController.h
+//  iStayHealthy
+//
+//  Created by Peter Schmidt on 22/06/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContainerViewController : UIViewController
+- (IBAction)transitionToContentController:(id)sender;
+- (IBAction)transitionToMenuController:(id)sender;
+@end
