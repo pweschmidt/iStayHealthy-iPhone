57b0aa4f07b1d94a5b038c200b0803affedb4e57
Peter Schmidt <peter.we.schmidt@gmail.com>
2013-10-14 22:47:11 +0100

    forgot that

--- Classes/Dashboard/PWESAxis.h
+++ Classes/Dashboard/PWESAxis.h
@@ -7,7 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PWESChartsConstants.h"
 
 @interface PWESAxis : NSObject
-+ (PWESAxis *)axisWithFrame:(CGRect)axisFrame;
+@property (nonatomic, assign) CGFloat lineWidth;
+@property (nonatomic, strong) UIColor *axisColor;
+@property (nonatomic, strong) NSString *axisTitle;
+@property (nonatomic, assign) AxisType axisTitleOrientation;
+@property (nonatomic, assign) CGFloat tickLength;
+@property (nonatomic, assign) CGFloat pxTickDistance;
+@property (nonatomic, assign) AxisType orientation;
+
+@property (nonatomic, strong) CALayer *axisLayer;
+
+- (id)initWithFrame:(CGRect)frame
+        orientation:(AxisType)orientation;
+- (void)configureAxis;
+- (void)show;
 @end
--- Classes/Dashboard/PWESAxis.m
+++ Classes/Dashboard/PWESAxis.m
@@ -7,7 +7,79 @@
 //
 
 #import "PWESAxis.h"
+#import "PWESUtils.h"
+
+@interface PWESAxis ()
+{
+    CGRect axisFrame;
+}
+@end
 
 @implementation PWESAxis
+
+- (id)initWithFrame:(CGRect)frame
+        orientation:(AxisType)orientation
+{
+    self = [super init];
+    if (nil != self)
+    {
+        _orientation = orientation;
+        _lineWidth = 2.0;
+        _pxTickDistance = 25.0;
+        _axisLayer = [CALayer layer];
+        _axisLayer.frame = frame;
+//        _axisLayer.backgroundColor = [UIColor whiteColor].CGColor;
+    }
+    return self;;
+}
+
+
+- (void)show
+{
+    if (nil == self.axisLayer)
+    {
+        return;
+    }
+    self.axisLayer.delegate = self;
+    [self.axisLayer setNeedsDisplay];
+}
+
+- (void)drawLayer:(CALayer *)layer
+        inContext:(CGContextRef)context
+{
+    CGPoint axisStart;
+    CGPoint axisEnd;
+    
+    CGRect frame = self.axisLayer.frame;
+    if (Vertical == self.orientation)
+    {
+        axisStart = CGPointMake(frame.origin.x + frame.size.width/2 + self.lineWidth/2, frame.origin.y);
+        axisEnd = CGPointMake(frame.origin.x + frame.size.width/2 + self.lineWidth/2, frame.size.height);
+//        [self addTickMarks];
+    }
+    else
+    {
+        axisStart = CGPointMake(frame.origin.x, frame.origin.y + frame.size.height/2 - self.lineWidth/2);
+        axisEnd = CGPointMake(frame.size.width, frame.origin.y + frame.size.height/2 - self.lineWidth/2);
+    }
+//    CGContextSaveGState(context);
+    CGContextSetLineCap(context, kCGLineCapSquare);
+    CGContextSetStrokeColorWithColor(context, self.axisColor.CGColor);
+    CGContextSetLineWidth(context, self.lineWidth);
+    CGFloat lineOffset = self.lineWidth / 2;
+    CGContextMoveToPoint(context, axisStart.x + lineOffset, axisStart.y + lineOffset);
+    CGContextAddLineToPoint(context, axisEnd.x + lineOffset, axisEnd.y + lineOffset);
+    CGContextStrokePath(context);
+//    CGContextRestoreGState(context);
+
+    /*
+    [[PWESUtils sharedInstance] drawLineWithContext:ctx start:axisStart end:axisEnd lineWidth:self.lineWidth cgColour:self.axisColor.CGColor];
+     */
+}
+
+- (void)addTickMarks
+{
+    
+}
 
 @end
