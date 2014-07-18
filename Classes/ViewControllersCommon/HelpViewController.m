699773be9da2f01ff583de99b221412c0f7ebe8a
Peter Schmidt <peter.schmidt@mendeley.com>
2014-07-18 14:58:51 +0100

    ok - got one help subject

--- Classes/ViewControllersCommon/HelpViewController.m
+++ Classes/ViewControllersCommon/HelpViewController.m
@@ -7,26 +7,49 @@
 //
 
 #import "HelpViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#import "HelpSubjects.h"
+#import "UILabel+Standard.h"
+
+static NSDictionary * helpSubjects()
+{
+    return @{ kHelpDataLoss : kHelpDataLossExplanation };
+}
+
 
 @interface HelpViewController ()
+@property (nonatomic, assign) BOOL isPopover;
 
 @end
 
 @implementation HelpViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initAsPopoverController
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+    self = [super init];
+    if (nil != self)
+    {
+        _isPopover = YES;
     }
     return self;
 }
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = DEFAULT_BACKGROUND;
+    __block CGFloat offset = 75;
+    NSDictionary *subjects = helpSubjects();
+    [subjects enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
+         UIView *header = [self headlineViewWithOffset:offset subject:key];
+         offset += 30;
+         UIView *text = [self contentViewWithOffset:offset explanation:obj];
+         offset += 20;
+         [self.view addSubview:header];
+         [self.view addSubview:text];
+     }];
+
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,15 +58,43 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+- (UIView *)headlineViewWithOffset:(CGFloat)offset subject:(NSString *)subject
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    UIView *view = [[UIView alloc] init];
+
+    view.frame = CGRectMake(20, offset, self.view.bounds.size.width - 40, 20);
+    view.backgroundColor = [UIColor clearColor];
+
+    UILabel *label = [UILabel standardLabel];
+    label.frame = CGRectMake(0, 0, view.frame.size.width, 20);
+    label.text = subject;
+    label.textAlignment = NSTextAlignmentLeft;
+
+    [view addSubview:label];
+
+    return view;
 }
-*/
+
+- (UIView *)contentViewWithOffset:(CGFloat)offset explanation:(NSString *)explanation
+{
+    UIView *view = [[UIView alloc] init];
+
+    view.frame = CGRectMake(20, offset, self.view.bounds.size.width - 40, 100);
+    view.backgroundColor = [UIColor clearColor];
+    view.layer.backgroundColor = [UIColor whiteColor].CGColor;
+    view.layer.cornerRadius = 10.0f;
+    view.layer.borderColor = [UIColor darkGrayColor].CGColor;
+    view.layer.borderWidth = 1.0f;
+
+    UILabel *label = [UILabel standardLabel];
+    label.frame = CGRectMake(10, 5, view.frame.size.width - 20, 100);
+    label.text = explanation;
+    label.textAlignment = NSTextAlignmentLeft;
+    label.numberOfLines = 0;
+    label.textColor = [UIColor blackColor];
+    [view addSubview:label];
+
+    return view;
+}
 
 @end
