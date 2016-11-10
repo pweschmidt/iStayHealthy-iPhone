fd62d3c5c330bb5c21b345a0dd0752ce3fc231a7
Schmidt, Peter (ELS) <p.schmidt@elsevier.com>
2016-11-10 16:52:09 +0000

    new dropbox controller

--- Classes/iOS8/UserInterface/DropboxSyncController.swift
+++ Classes/iOS8/UserInterface/DropboxSyncController.swift
@@ -11,86 +11,188 @@ import SwiftyDropbox
 
 class DropboxSyncController: UITableViewController {
 
+    let tmpURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+    let dropBoxBackupPath = "/iStayHealthy/iStayHealthy.isth"
+    let dropBoxUploadPath = "/iStayHealthy/toDropBox.xml"
+    let dateFormatString = "ddMMMyyyy'_'HHmmss"
+
+    var downloadPath: URL {
+        get {
+            return tmpURL.appendingPathComponent("fromDropBox.xml")
+        }
+    }
+
+    var uploadPath: URL {
+        get {
+            return tmpURL.appendingPathComponent("toDropBox.xml")
+        }
+    }
+
+    var backupFile: URL {
+        get {
+            return tmpURL.appendingPathComponent("iStayHealthy.isth")
+        }
+    }
+    
+    var revBackupFile: String {
+        get {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = dateFormatString
+            let formattedDateString = dateFormatter.string(from: Date())
+            return "/iStayHealthy/iStayHealthy_"+formattedDateString+".isth"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "syncCell")
+        view.backgroundColor = kDefaultBackground
+        tableView.backgroundColor = kDefaultBackground
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "syncCell", for: indexPath)
+        cell.contentView.backgroundColor = UIColor.white
+        cell.textLabel?.textColor = kTextColour
+        if 0 == indexPath.section {
+            if nil != DropboxClientsManager.authorizedClient {
+                cell.accessoryType = .disclosureIndicator
+            }
+            else {
+                cell.textLabel?.textColor = UIColor.lightGray
+            }
+            cell.textLabel?.text = NSLocalizedString("Sync with Dropbox", comment: "")
+        }
+        else {
+            if nil != DropboxClientsManager.authorizedClient {
+                cell.textLabel?.text = NSLocalizedString("Unlink Dropbox", comment: "")
+            }
+            else {
+                cell.textLabel?.text = NSLocalizedString("Link with Dropbox", comment: "")
+            }
+            cell.accessoryType = .disclosureIndicator
+        }
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if 0 == indexPath.section {
+            if nil != DropboxClientsManager.authorizedClient {
+                restore()
+                backup()
+            }
+        }
+        else {
+            
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    fileprivate func backup() {
+        let xmlWriter = CoreXMLWriter()
+        xmlWriter.write { (xmlString, error) in
+            if let xmlString = xmlString {
+                if let data = xmlString.data(using: .utf8, allowLossyConversion: false) {
+                    do {
+                        try data.write(to: self.uploadPath)
+                    }catch {
+                        PWESAlertHandler.alertHandler.showAlertViewWithOKButton(NSLocalizedString("Error", comment: ""), message: NSLocalizedString("Error writing data to tmp directory", comment: ""), presentingController: self)
+                    }
+                    if let client = DropboxClientsManager.authorizedClient {
+                        client.files.upload(path: self.dropBoxUploadPath, input: data).response(completionHandler: { (uploadResponse, uploadError) in
+                            if nil != uploadResponse {
+                                self.moveUploadedFiles()
+                            }
+                            else if nil != uploadError {
+                                PWESAlertHandler.alertHandler.showAlertViewWithOKButton(NSLocalizedString("Error Uploading to Dropbox", comment: ""), message: NSLocalizedString("There was an error uploading data to Dropbox.", comment: ""), presentingController: self)
+                                
+                            }
+                        })
+                    }
+                }
+                
+            }
+            else if let error = error {
+                PWESAlertHandler.alertHandler.showAlertViewWithOKButton(NSLocalizedString("Error", comment: ""), message: NSLocalizedString(error.localizedDescription, comment: ""), presentingController: self)
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    fileprivate func moveUploadedFiles() {
+        if let client = DropboxClientsManager.authorizedClient {
+            client.files.search(path: "/iStayHealthy", query: "iStayHealthy.isth").response(completionHandler: { (searchResults, error) in
+                if let searchResults = searchResults {
+                    if searchResults.matches.count > 0 {
+                        client.files.move(fromPath: self.dropBoxBackupPath, toPath: self.revBackupFile).response(completionHandler: { (response, moveError) in
+                            self.moveUploadedFile()
+                        })
+                    }
+                }
+                else if nil != error {
+                    
+                }
+                else {
+                    self.moveUploadedFile()
+                }
+            })
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    fileprivate func moveUploadedFile() {
+        if let client = DropboxClientsManager.authorizedClient {
+            client.files.move(fromPath: dropBoxUploadPath, toPath: dropBoxBackupPath).response(completionHandler: { (response, error) in
+                if nil != response {
+                    
+                }
+                else if nil != error {
+                    
+                }
+            })
+        }
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    fileprivate func restore() {
+        let destination: (URL, HTTPURLResponse) -> URL = { temporaryURL, response in
+            return self.downloadPath
+        }
+        if let client = DropboxClientsManager.authorizedClient {
+            client.files.download(path: dropBoxBackupPath).response(completionHandler: { (response, error) in
+                if let response = response {
+                    
+                }
+                else if let error = error {
+                    
+                }
+            }).progress({ (progressData) in
+                _ = progressData.totalUnitCount
+            })
+        }
     }
-    */
+    
+    
+    fileprivate func link() {
+        DropboxClientsManager.authorizeFromController(UIApplication.shared, controller: self, openURL: {(url: URL) -> Void in UIApplication.shared.openURL(url)})
+    }
+    
+    fileprivate func unlink() {
+        DropboxClientsManager.unlinkClients()
+    }
+    
+    
+    
 
 }
